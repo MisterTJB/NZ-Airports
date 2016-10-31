3a04a8f7e4e07ba865cf955f2ab7d2429c150295
Tim <tim.bathgate@gmail.com>
2016-10-31 16:11:07 +1300

    Add limited airport data

---
+++ data/airports.js
@@ -0,0 +1,18 @@
+var airportsData = [
+  {
+    "airportCode": "NZCH",
+    "airportName": "Christchurch International Airport",
+    "latitude": -43.486400,
+    "longitude": 172.536900,
+    "maxGroupRating": 8,
+    "permission": "–"
+  },
+  {
+    "airportCode": "NZRT",
+    "airportName": "Rangiora Aerodrome",
+    "latitude": -43.2878,
+    "longitude": 172.5413,
+    "maxGroupRating": 8,
+    "permission": "Available for general use without the permission of the operator"
+  }
+]
