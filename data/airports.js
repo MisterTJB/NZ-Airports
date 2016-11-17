1e01cb2a9d883baed018ce138a275a5ea6d88626
Tim <tim.bathgate@gmail.com>
2016-11-18 09:03:33 +1300

    Add airports

--- data/airports.js
+++ data/airports.js
@@ -52,5 +52,176 @@ var airportsData = [
     "elevation": 1777,
     "maxGroupRating": 6,
     "permission": "Available for limited use subject to the prior agreement of the operator"
+  },
+  {
+    "airportCode": "NZME",
+    "airportName": "Mercer",
+    "latitude": -37.259167,
+    "longitude": 175.114722,
+    "elevation": 30,
+    "maxGroupRating": 8,
+    "permission": "Available for limited use subject to the prior agreement of the operator"
+  },
+  {
+    "airportCode": "NZOT",
+    "airportName": "Otaki",
+    "latitude": -40.783611,
+    "longitude": 175.151111,
+    "elevation": 100,
+    "maxGroupRating": 6,
+    "permission": "Available for limited use subject to the prior agreement of the operator"
+  },
+  {
+    "airportCode": "NZRC",
+    "airportName": "Ryans Creek",
+    "latitude": -46.899722,
+    "longitude": 168.101944,
+    "elevation": 288,
+    "maxGroupRating": 4,
+    "permission": "Available for limited use subject to the prior agreement of the operator"
+  },
+  {
+    "airportCode": "NZTI",
+    "airportName": "Taieri",
+    "latitude": -45.86,
+    "longitude": 170.358333,
+    "elevation": 85,
+    "maxGroupRating": 7,
+    "permission": "Available for limited use with landing fee $10 if paid on the day (payable at pump or office), or $30 if invoiced"
+  },
+  {
+    "airportCode": "NZTT",
+    "airportName": "Te Kuiti",
+    "latitude": -38.303333,
+    "longitude": 175.146944,
+    "elevation": 160,
+    "maxGroupRating": 6,
+    "permission": "Available for general use without the permission of the operator"
+  },
+  {
+    "airportCode": "NZWV",
+    "airportName": "Waihi Beach",
+    "latitude": -37.43,
+    "longitude": 175.951944,
+    "elevation": 4,
+    "maxGroupRating": 4,
+    "permission": "Available only with the prior permission of the operator"
+  },
+  {
+    "airportCode": "NZWF",
+    "airportName": "Wanaka",
+    "latitude": -44.722222,
+    "longitude": 169.245556,
+    "elevation": 1142,
+    "maxGroupRating": 8,
+    "permission": "Available for general use without the permission of the operator"
+  },
+  {
+    "airportCode": "NZWN",
+    "airportName": "Wellington",
+    "latitude": -41.327222,
+    "longitude": 174.805278,
+    "elevation": 41,
+    "maxGroupRating": 8,
+    "permission": "Available for general use without the permission of the operator"
+  },
+  {
+    "airportCode": "NZAA",
+    "airportName": "Auckland",
+    "latitude": -37.008056,
+    "longitude": 174.791667,
+    "elevation": 23,
+    "maxGroupRating": 8,
+    "permission": "Available for general use without the permission of the operator"
+  },
+  {
+    "airportCode": "NZCX",
+    "airportName": "Coromandel",
+    "latitude": -36.791667,
+    "longitude": 175.508611,
+    "elevation": 13,
+    "maxGroupRating": 4,
+    "permission": "Landing fees payable at all times"
+  },
+  {
+    "airportCode": "NZFE",
+    "airportName": "Fernside Fields",
+    "latitude": -43.3175,
+    "longitude": 172.511667,
+    "elevation": 195,
+    "maxGroupRating": 5,
+    "permission": "Only available for use with prior permission of the operator"
+  },
+  {
+    "airportCode": "NZFT",
+    "airportName": "Flat Point",
+    "latitude": -41.243056,
+    "longitude": 175.960278,
+    "elevation": 25,
+    "maxGroupRating": 3,
+    "permission": "Available for general use subject to prior agreement of the operator"
+  },
+  {
+    "airportCode": "NZGY",
+    "airportName": "Glenorchy",
+    "latitude": -44.871667,
+    "longitude": 168.3975,
+    "elevation": 1255,
+    "maxGroupRating": 3,
+    "permission": "Available for general use without the permission of the operator"
+  },
+  {
+    "airportCode": "NZHT",
+    "airportName": "Haast",
+    "latitude": -43.866667,
+    "longitude": 169.040278,
+    "elevation": 22,
+    "maxGroupRating": 5,
+    "permission": "Commercial activity not permitted"
+  },
+  {
+    "airportCode": "NZKI",
+    "airportName": "Kaikoura",
+    "latitude": -42.425,
+    "longitude": 173.605278,
+    "elevation": 19,
+    "maxGroupRating": 4,
+    "permission": "Available for general use without the permission of the operator"
+  },
+  {
+    "airportCode": "NZKF",
+    "airportName": "Kaipara Flats",
+    "latitude": -36.406389,
+    "longitude": 174.587222,
+    "elevation": 110,
+    "maxGroupRating": 6,
+    "permission": "Commercial by prior arrangement only"
+  },
+  {
+    "airportCode": "NZKY",
+    "airportName": "Kowhai",
+    "latitude": -39.845278,
+    "longitude": 176.421389,
+    "elevation": 810,
+    "maxGroupRating": 6,
+    "permission": "Available for general use without the permission of the operator"
+  },
+  {
+    "airportCode": "NZLA",
+    "airportName": "Loburn Abbey",
+    "latitude": -43.233611,
+    "longitude": 172.494167,
+    "elevation": 380,
+    "maxGroupRating": 5,
+    "permission": "Available for limited use, prior notification of operator requested"
+  },
+  {
+    "airportCode": "NZVL",
+    "airportName": "Mandeville",
+    "latitude": -45.990278,
+    "longitude": 168.812222,
+    "elevation": 335,
+    "maxGroupRating": 7,
+    "permission": "Available for limited use subject to prior agreement of the operator"
   }
 ]
