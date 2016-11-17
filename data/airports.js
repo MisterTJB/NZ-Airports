9e403d0662170d7903183f785e81c84b04c5ff9e
Tim <tim.bathgate@gmail.com>
2016-11-17 15:08:14 +1300

    Add markers with infoboxes

--- data/airports.js
+++ data/airports.js
@@ -1,7 +1,7 @@
 var airportsData = [
   {
     "airportCode": "NZCH",
-    "airportName": "Christchurch International Airport",
+    "airportName": "Christchurch",
     "latitude": -43.486400,
     "longitude": 172.536900,
     "maxGroupRating": 8,
@@ -9,10 +9,46 @@ var airportsData = [
   },
   {
     "airportCode": "NZRT",
-    "airportName": "Rangiora Aerodrome",
+    "airportName": "Rangiora",
     "latitude": -43.2878,
     "longitude": 172.5413,
-    "maxGroupRating": 5,
+    "maxGroupRating": 8,
     "permission": "Available for general use without the permission of the operator"
+  },
+  {
+    "airportCode": "NZLX",
+    "airportName": "Alexandra",
+    "latitude": -45.211667,
+    "longitude": 169.373333,
+    "elevation": 752,
+    "maxGroupRating": 8,
+    "permission": "Available for general use without the permission of the operator"
+  },
+  {
+    "airportCode": "NZAR",
+    "airportName": "Ardmore",
+    "latitude": -37.029722,
+    "longitude": 174.973333,
+    "elevation": 111,
+    "maxGroupRating": 8,
+    "permission": "Available for general use without the permission of the operator"
+  },
+  {
+    "airportCode": "NZAS",
+    "airportName": "Ashburton",
+    "latitude": -43.903333,
+    "longitude": 171.796667,
+    "elevation": 298,
+    "maxGroupRating": 8,
+    "permission": "Available for general use without the permission of the operator"
+  },
+  {
+    "airportCode": "NZGT",
+    "airportName": "Glentanner",
+    "latitude": -43.907778,
+    "longitude": 170.129167,
+    "elevation": 1777,
+    "maxGroupRating": 6,
+    "permission": "Available for limited use subject to the prior agreement of the operator"
   }
 ]
--- js/app.js
+++ js/app.js
@@ -1,15 +1,99 @@
 $(document).foundation()
 
+var INFO_TEMPLATE = "<div class='infoWindow'><h1>%AIRPORT% (%CODE%)</h1><h3>Elevation: %ELEVATION% ft</h3><h4>Group Rating: %GROUP%</h4><p>%FORECAST%</p>";
+
 function AirportsViewModel(data) {
   var self = this;
   self.airports = data;
   self.groupRating = ko.observable(6);
   self.filteredAirports = ko.computed(function() {
-    var filtered = ko.utils.arrayFilter(self.airports, function(airport){
+    return ko.utils.arrayFilter(self.airports, function(airport){
       return airport.maxGroupRating >= self.groupRating();
+    }).sort(function (left, right){
+      return left.airportCode < right.airportCode ? -1 : 1;
     });
-    return filtered;
+  });
+  self.filterMarkers = ko.computed(function() {
+    self.airports.forEach(function(airport){
+      if (airport.maxGroupRating >= self.groupRating() && airport.marker){
+        airport.marker.setVisible(true);
+      } else if (airport.marker) {
+        airport.marker.setVisible(false);
+      }
+    });
+    if (infoWindow) {
+      infoWindow.close();
+    }
+  });
+  self.calloutMarker = function(airport) {
+    google.maps.event.trigger(airport.marker, 'click');
+  };
+
+}
+
+var viewModel = new AirportsViewModel(airportsData);
+var infoWindow;
+ko.applyBindings(viewModel);
+
+function createMarkers(map){
+  infoWindow = new google.maps.InfoWindow();
+  google.maps.event.addListener(infoWindow,'closeclick',function(){
+    viewModel.airports.forEach(function(airport){
+      airport.marker.setAnimation(null);
+    });
+ });
+  viewModel.airports.forEach(function(airport){
+    var marker = new google.maps.Marker({
+      position: {lat: airport.latitude, lng: airport.longitude},
+      map: map
+    });
+    google.maps.event.addListener(marker, 'click', function () {
+      buildInfoWindowForAirport(airport, infoWindow);
+      viewModel.airports.forEach(function(airport){
+        airport.marker.setAnimation(null);
+      });
+      if (marker.getAnimation() !== null) {
+          marker.setAnimation(null);
+        } else {
+          marker.setAnimation(google.maps.Animation.BOUNCE);
+        }
+    });
+    airport.marker = marker;
   });
 }
 
-ko.applyBindings(new AirportsViewModel(airportsData));
+function buildInfoWindowForAirport(airport, infoWindow) {
+  var endpoint = "http://api.wunderground.com/api/30d55959c65fe197/forecast/q/%LAT%,%LON%.json"
+  endpoint = endpoint.replace("%LAT%", airport.latitude);
+  endpoint = endpoint.replace("%LON%", airport.longitude);
+  infoWindow.setContent("<h1>" + airport.airportName + "</h1>");
+  infoWindow.open(map, airport.marker);
+  var contentString;
+  $.ajax( {
+    url: endpoint,
+    dataType: 'jsonp',
+    type: 'GET'}).done(function(data) {
+      var forecast = data.forecast.txt_forecast.forecastday[0].fcttext_metric;
+      contentString = INFO_TEMPLATE.replace("%AIRPORT%", airport.airportName);
+      contentString = contentString.replace("%CODE%", airport.airportCode);
+      contentString = contentString.replace("%GROUP%", airport.maxGroupRating);
+      contentString = contentString.replace("%ELEVATION%", airport.elevation);
+      if (forecast) {
+        contentString = contentString.replace("%FORECAST%", forecast);
+      } else {
+        contentString = contentString.replace("%FORECAST%", "No weather data available");
+      }
+    }).fail(function(){
+      contentString = contentString.replace("%FORECAST%", "This is embarassing: a networking issue prevented me from retrieving weather information");
+    }).always(function(){
+      infoWindow.setContent(contentString);
+    });
+}
+
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 6,
+    center: {lat: -40.9006, lng: 174.8860}
+  });
+  createMarkers(map);
+}
